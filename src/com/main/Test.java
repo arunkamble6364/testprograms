a45a649da09a8c535f66692a47a8885a510a982b
arunkamble <arunkamble6364@gmail.com>
2022-09-29 12:10:21 +0530

    commit

---
+++ src/com/main/Test.java
@@ -0,0 +1,18 @@
+package com.main;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World");
+		
+		
+		for(int i=0; i<=5;i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
